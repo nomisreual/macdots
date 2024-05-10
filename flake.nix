89e5db10141476ef248c1d18a92659e334257814
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-05-10 20:41:16 +0200

    feat: preparing home manager

--- flake.nix
+++ flake.nix
@@ -5,27 +5,28 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, home-manager, nix-darwin, nixpkgs }:
   let
     configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [
-          pkgs.vim
-          pkgs.htop
-	  pkgs.neovim
-          pkgs.python311
-          pkgs.poetry
+        with pkgs; [
+          vim
+          htop
+          neovim
+          python311
+          nodejs_22
+          poetry
+          tmux
         ];
 
       # Homebrew
       homebrew = {
         enable = true;
         casks = [
-          "rectangle"
           "1password"
           "dbeaver-community"
           "kitty"
@@ -39,37 +40,73 @@
           "proton-drive"
           "pdf-expert"
         ];
+        onActivation = {
+          autoUpdate = true;
+          cleanup = "zap";
+        };
       };
-      homebrew.onActivation.autoUpdate = true;
-      homebrew.onActivation.cleanup = "zap";
+
       # Yabai
-      # services.yabai.enable = true;
-      # services.yabai.config = {
-      #   layout = "bsp";
-      #   focus_follows_mouse = "autoraise";
-      #   mouse_follows_focus = "off";
-      #   window_placement    = "second_child";
-      #   window_opacity      = "off";
-      #   window_gap          = 0;
-      # };
-      # services.yabai.extraConfig = ''
-      #   yabai -m config external_bar all:32:0
-      # '';
+      services.yabai = {
+        enable = true;
+        enableScriptingAddition = true;
+        config = {
+          layout = "bsp";
+        };
+        extraConfig = ''
+          yabai -m rule --add app="^Firefox$" space=2
+          yabai -m rule --add app="^Warp$" space=1
+          yabai -m config external_bar all:40:0
 
-
-      system.defaults = {
-        dock.autohide = false;
-        NSGlobalDomain._HIHideMenuBar = false;
-        # dock.mru-spaces = false;
-        # finder.AppleShowAllExtensions = true;
-        # finder.FXPreferredViewStyle = "clmv";
-        # loginwindow.LoginwindowText = "nixcademy.com";
-        # screencapture.location = "~/Pictures/screenshots";
-        # screensaver.askForPasswordDelay = 10;
+        '';
       };
 
-      # Sketchy Bar
-      # services.sketchybar.enable = true;
+      # SKHD
+      services.skhd = {
+        enable = true;
+        skhdConfig = ''
+          ctrl - h: yabai -m window --focus west
+          ctrl - j: yabai -m window --focus south
+          ctrl - k: yabai -m window --focus north
+          ctrl - l: yabai -m window --focus east
+
+          cmd - h: yabai -m space --focus prev
+          cmd - l: yabai -m space --focus next
+          cmd - z : yabai -m space --focus recent
+          cmd - 1 : yabai -m space --focus 1
+          cmd - 2 : yabai -m space --focus 2
+          cmd - 3 : yabai -m space --focus 3
+          cmd - 4 : yabai -m space --focus 4
+          cmd - 5 : yabai -m space --focus 5
+          cmd - 6 : yabai -m space --focus 6
+          cmd - 7 : yabai -m space --focus 7
+          cmd - 8 : yabai -m space --focus 8
+          cmd - 9 : yabai -m space --focus 9
+          cmd - 0 : yabai -m space --focus 10
+
+          cmd + shift - 1 : yabai -m window --space  1; yabai -m space --focus 1
+          cmd + shift - 2 : yabai -m window --space  2; yabai -m space --focus 2
+          cmd + shift - 3 : yabai -m window --space  3; yabai -m space --focus 3
+          cmd + shift - 4 : yabai -m window --space  4; yabai -m space --focus 4
+          cmd + shift - 5 : yabai -m window --space  5; yabai -m space --focus 5
+          cmd + shift - 6 : yabai -m window --space  6; yabai -m space --focus 6
+          cmd + shift - 7 : yabai -m window --space  7; yabai -m space --focus 7
+          cmd + shift - 8 : yabai -m window --space  8; yabai -m space --focus 8
+          cmd + shift - 9 : yabai -m window --space  9; yabai -m space --focus 9
+          cmd + shift - 0 : yabai -m window --space 10; yabai -m space --focus 10
+        '';
+      };
+
+      # Sketchybar
+      services.sketchybar = {
+        enable = true;
+      };
+
+      # Hide dock and menu bar:
+      system.defaults = {
+        dock.autohide = true;
+        NSGlobalDomain._HIHideMenuBar = true;
+      };
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
@@ -85,13 +122,13 @@
       fonts = {
         fontDir.enable = true;
         fonts = with pkgs; [
-          (nerdfonts.override {fonts = ["FantasqueSansMono"];})
+          (nerdfonts.override {fonts = ["FantasqueSansMono" "Hack"];})
+          font-awesome_5
         ];
       };
 
       # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-      # programs.fish.enable = true;
+      programs.zsh.enable = true;
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
