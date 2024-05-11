cad078772070d654a6004b4d1ed95aa6f8a9a742
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-05-11 13:50:30 +0200

    feat: removed tiler

--- flake.nix
+++ flake.nix
@@ -47,65 +47,65 @@
       };
 
       # Yabai
-      services.yabai = {
-        enable = true;
-        enableScriptingAddition = true;
-        config = {
-          layout = "bsp";
-        };
-        extraConfig = ''
-          yabai -m rule --add app="^Firefox$" space=2
-          yabai -m rule --add app="^Warp$" space=1
-          yabai -m config external_bar all:40:0
-
-        '';
-      };
-
-      # SKHD
-      services.skhd = {
-        enable = true;
-        skhdConfig = ''
-          ctrl - h: yabai -m window --focus west
-          ctrl - j: yabai -m window --focus south
-          ctrl - k: yabai -m window --focus north
-          ctrl - l: yabai -m window --focus east
-
-          cmd - h: yabai -m space --focus prev
-          cmd - l: yabai -m space --focus next
-          cmd - z : yabai -m space --focus recent
-          cmd - 1 : yabai -m space --focus 1
-          cmd - 2 : yabai -m space --focus 2
-          cmd - 3 : yabai -m space --focus 3
-          cmd - 4 : yabai -m space --focus 4
-          cmd - 5 : yabai -m space --focus 5
-          cmd - 6 : yabai -m space --focus 6
-          cmd - 7 : yabai -m space --focus 7
-          cmd - 8 : yabai -m space --focus 8
-          cmd - 9 : yabai -m space --focus 9
-          cmd - 0 : yabai -m space --focus 10
-
-          cmd + shift - 1 : yabai -m window --space  1; yabai -m space --focus 1
-          cmd + shift - 2 : yabai -m window --space  2; yabai -m space --focus 2
-          cmd + shift - 3 : yabai -m window --space  3; yabai -m space --focus 3
-          cmd + shift - 4 : yabai -m window --space  4; yabai -m space --focus 4
-          cmd + shift - 5 : yabai -m window --space  5; yabai -m space --focus 5
-          cmd + shift - 6 : yabai -m window --space  6; yabai -m space --focus 6
-          cmd + shift - 7 : yabai -m window --space  7; yabai -m space --focus 7
-          cmd + shift - 8 : yabai -m window --space  8; yabai -m space --focus 8
-          cmd + shift - 9 : yabai -m window --space  9; yabai -m space --focus 9
-          cmd + shift - 0 : yabai -m window --space 10; yabai -m space --focus 10
-        '';
-      };
-
-      # Sketchybar
-      services.sketchybar = {
-        enable = true;
-      };
-
+      # services.yabai = {
+      #   enable = true;
+      #   enableScriptingAddition = true;
+      #   config = {
+      #     layout = "bsp";
+      #   };
+      #   extraConfig = ''
+      #     yabai -m rule --add app="^Firefox$" space=2
+      #     yabai -m rule --add app="^Warp$" space=1
+      #     yabai -m config external_bar all:40:0
+      #
+      #   '';
+      # };
+      #
+      # # SKHD
+      # services.skhd = {
+      #   enable = true;
+      #   skhdConfig = ''
+      #     ctrl - h: yabai -m window --focus west
+      #     ctrl - j: yabai -m window --focus south
+      #     ctrl - k: yabai -m window --focus north
+      #     ctrl - l: yabai -m window --focus east
+      #
+      #     cmd - h: yabai -m space --focus prev
+      #     cmd - l: yabai -m space --focus next
+      #     cmd - z : yabai -m space --focus recent
+      #     cmd - 1 : yabai -m space --focus 1
+      #     cmd - 2 : yabai -m space --focus 2
+      #     cmd - 3 : yabai -m space --focus 3
+      #     cmd - 4 : yabai -m space --focus 4
+      #     cmd - 5 : yabai -m space --focus 5
+      #     cmd - 6 : yabai -m space --focus 6
+      #     cmd - 7 : yabai -m space --focus 7
+      #     cmd - 8 : yabai -m space --focus 8
+      #     cmd - 9 : yabai -m space --focus 9
+      #     cmd - 0 : yabai -m space --focus 10
+      #
+      #     cmd + shift - 1 : yabai -m window --space  1; yabai -m space --focus 1
+      #     cmd + shift - 2 : yabai -m window --space  2; yabai -m space --focus 2
+      #     cmd + shift - 3 : yabai -m window --space  3; yabai -m space --focus 3
+      #     cmd + shift - 4 : yabai -m window --space  4; yabai -m space --focus 4
+      #     cmd + shift - 5 : yabai -m window --space  5; yabai -m space --focus 5
+      #     cmd + shift - 6 : yabai -m window --space  6; yabai -m space --focus 6
+      #     cmd + shift - 7 : yabai -m window --space  7; yabai -m space --focus 7
+      #     cmd + shift - 8 : yabai -m window --space  8; yabai -m space --focus 8
+      #     cmd + shift - 9 : yabai -m window --space  9; yabai -m space --focus 9
+      #     cmd + shift - 0 : yabai -m window --space 10; yabai -m space --focus 10
+      #   '';
+      # };
+      #
+      # # Sketchybar
+      # services.sketchybar = {
+      #   enable = true;
+      # };
+      #
       # Hide dock and menu bar:
       system.defaults = {
         dock.autohide = true;
-        NSGlobalDomain._HIHideMenuBar = true;
+        NSGlobalDomain._HIHideMenuBar = false;
       };
 
       # Auto upgrade nix package and the daemon service.
